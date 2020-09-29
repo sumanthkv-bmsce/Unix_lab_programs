046168180a2f0b28b78358a654584924f87249d9
sumanthkv-bmsce <sumanth.cs18@bmsce.ac.in>
2020-09-29 10:53:37 +0530

    updated area of the circle

--- Area_of_the_circle.sh
+++ Area_of_the_circle.sh
@@ -3,13 +3,3 @@ read r
 area=$(echo "3.14*$r*$r" | bc  )
 circum=$(echo "3.14*2*$r" | bc)
 echo "area of the circle is             " $area
-echo "Enter the radious of the circle"
-read r
-area=$(echo "3.14*$r*$r" | bc  )
-circum=$(echo "3.14*2*$r" | bc)
-echo "area of the circle is             " $area
-echo "Enter the radious of the circle"
-read r
-area=$(echo "3.14*$r*$r" | bc  )
-circum=$(echo "3.14*2*$r" | bc)
-echo "area of the circle is             " $area
