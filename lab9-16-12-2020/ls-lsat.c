ff7fafc89079bb0591034012029b487ccbd8bd85
sumanthkv-bmsce <44916968+sumanthkv-bmsce@users.noreply.github.com>
2020-12-16 10:55:34 +0530

    Create ls-lsat.c

---
+++ lab9-16-12-2020/ls-lsat.c
@@ -0,0 +1,46 @@
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#define BUF_SIZE 8192
+
+int main(int argc , char* argv[])
+{
+  if (argc!=2)
+  {
+  	fprintf(stderr,"Usage: lstat_stat file1 ");
+  	return 1;
+  }
+
+  struct stat fileStat; 
+  if (lstat(argv[1],&fileStat) < 0) 
+  {
+  	printf("error opening file");
+  	return 1;
+  }
+ 
+    printf("Information for %s\n",argv[1]);
+    rintf("---------------------------\n");
+    printf("File Size: \t\t%d bytes\n",fileStat.st_size);
+    printf("Number of Links: \t%d\n",fileStat.st_nlink);
+    printf("File inode: \t\t%d\n",fileStat.st_ino);
+    printf("File Permissions: \t");
+    printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+    printf("\n\n");
+
+
+
+
+}
